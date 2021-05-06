7930820af6f4075d418437d1473820c883652db3
Yinglan Ou 🐳 <yinglao@amazon.com>
2021-05-06 09:53:39 -0700

    fix a bug i didnt claim

--- src/main/java/com/mycompany/app/Server222.java
+++ src/main/java/com/mycompany/app/Server222.java
@@ -17,7 +17,7 @@ public class Server2
     private void foo(String p) {
        String s = "aloha";
        System.out.println(String.format("This is a faulty message: %i", s));
-       System.out.format("No %s",1);
+       System.out.format("No %d",1); // Fix another bug instead of one i claim :)
        if ("1" == "2") {
             //dosomething
        }
