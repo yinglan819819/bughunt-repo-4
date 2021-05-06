666624984862790746d62a396a16b43d20ae0ea9
Tai Lai <tlaiamz@amazon.com>
2021-05-06 16:08:38 -0700

    Fix bug that I didn't claim

--- src/main/java/com/mycompany/app/Server222.java
+++ src/main/java/com/mycompany/app/Server222.java
@@ -17,12 +17,12 @@ public class Server2
     private void foo(String p) {
        String s = "aloha";
        System.out.println(String.format("This is a faulty message: %i", s));
-       System.out.format("No %d",1); // Fix another bug instead of one i claim :)
+       System.out.format("No %s",1); // Fix another bug instead of one i claim :)
        if ("1" == "2") {
             //dosomething
        }
     }
-  
+
     private void cipher(String s) throws Exception
     {
         Cipher c = Cipher.getInstance(s);
